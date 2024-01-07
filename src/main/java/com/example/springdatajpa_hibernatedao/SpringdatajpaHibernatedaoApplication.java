269f892b0d1280300495a631708d925f47da29d0
mrdevsam <mdshoykotislam05@gmail.com>
2024-01-07 10:25:48 +0000

    first commit

---
+++ src/main/java/com/example/springdatajpa_hibernatedao/SpringdatajpaHibernatedaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpa_hibernatedao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaHibernatedaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaHibernatedaoApplication.class, args);
+	}
+
+}
