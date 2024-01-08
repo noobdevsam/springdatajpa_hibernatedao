b308d2da5ccc0336ccd6dc8de4c6e468f0331fff
mrdevsam <mdshoykotislam05@gmail.com>
2024-01-08 08:42:12 +0000

    getById with hibernate

--- src/main/java/com/example/springdatajpa_hibernatedao/dao/AuthorDaoImpl.java
+++ src/main/java/com/example/springdatajpa_hibernatedao/dao/AuthorDaoImpl.java
@@ -2,13 +2,20 @@ package com.example.springdatajpa_hibernatedao.dao;
 
 import com.example.springdatajpa_hibernatedao.model.Author;
 import org.springframework.stereotype.Component;
+import jakarta.persistence.*;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
 
+	private final EntityManagerFactory emf;
+
+	public AuthorDaoImpl(EntityManagerFactory emf) {
+		this.emf = emf;
+	}
+
 	@Override
 	public Author getById(Long id) {
-		return null;
+		return getEntityManager().find(Author.class, id); // here [.find(entity_object, primary_key)]
 	}
 
 	@Override
@@ -29,5 +36,10 @@ public class AuthorDaoImpl implements AuthorDao {
 	@Override
 	public void deleteAuthorById(Long id) {
 		
+	}
+
+	// helper method to get entity-manager from entity-manager-factory
+	private EntityManager getEntityManager() {
+		return emf.createEntityManager();
 	}
 }
---
+++ src/test/java/com/example/springdatajpa_hibernatedao/DaoIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.example.springdatajpa_hibernatedao;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.springdatajpa_hibernatedao.dao.*;
+import com.example.springdatajpa_hibernatedao.model.*;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DataJpaTest
+@ComponentScan(basePackages = {"com.example.springdatajpa_hibernatedao"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class DaoIntegrationTest {
+
+	@Autowired
+	AuthorDaoImpl authorDaoImpl;
+
+	@Test
+	void test_getauthorbyid() {
+		Author author = authorDaoImpl.getById(1L);
+		assertThat(author).isNotNull();
+	}
+}
