0d6682c46c40e438638ecb712ef7c3936adc41e8
mrdevsam <mdshoykotislam05@gmail.com>
2024-01-10 21:46:06 +0000

    updateAuthor with hibernate

--- src/test/java/com/example/springdatajpa_hibernatedao/DaoIntegrationTest.java
+++ src/test/java/com/example/springdatajpa_hibernatedao/DaoIntegrationTest.java
@@ -21,19 +21,22 @@ public class DaoIntegrationTest {
 	AuthorDaoImpl authorDaoImpl;
 
 	@Test
-	void test_getauthorbyid() {
+	void test_get_author_by_id() {
+		System.out.println("get-by-id");
 		Author author = authorDaoImpl.getById(1L);
 		assertThat(author).isNotNull();
 	}
 
 	@Test
-	void test_findauthorbyname() {
+	void test_find_author_by_name() {
+		System.out.println("find-author-by-name");
 		Author author = authorDaoImpl.findAuthorByName("Craig", "Walls");
 		assertThat(author).isNotNull();
 	}
 
 	@Test
-	void test_savenewauthor() {
+	void test_save_new_author() {
+		System.out.println("save-new-author");
 		Author author = new Author();
 		author.setFirstName("Sam");
 		author.setLastName("K");
@@ -42,5 +45,19 @@ public class DaoIntegrationTest {
 
 		assertThat(saved).isNotNull();
 		assertThat(saved.getId()).isNotNull();
+	}
+
+	@Test
+	void test_update_author() {
+		System.out.println("update-author");
+		Author author = new Author();
+		author.setFirstName("Sam");
+		author.setLastName("Kn");
+		Author saved = authorDaoImpl.saveNewAuthor(author);
+
+		saved.setLastName("Knight");
+		Author updated = authorDaoImpl.updateAuthor(saved);
+
+		assertThat(updated.getLastName()).isEqualTo("Knight");
 	}
 }
